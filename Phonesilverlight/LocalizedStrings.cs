c3185e458b2955d15bf11303d0ae58dc12794286
Abeare <abeare21@hotmail.com>
2016-05-27 00:39:22 +0300

    stop delete me

---
+++ Phonesilverlight/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Phonesilverlight.Resources;
+
+namespace Phonesilverlight
+{
+    /// <summary>
+    /// Предоставляет доступ к строковым ресурсам.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
